80fb613deb443d9b72c163250f535513a26912a4
Yan Rymarchik <tricky.lynx@gmail.com>
2017-11-19 18:32:10 +0300

    dev v1.0  ready

--- TimeActualisationXMLTool/MainWindow.xaml.cs
+++ TimeActualisationXMLTool/MainWindow.xaml.cs
@@ -1,29 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using System.Globalization;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace TimeActualisationXMLTool
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public string settedPath;
+        public static DateTime settedDateTime;
         public MainWindow()
         {
+            settedPath = "none";
             InitializeComponent();
-            //Start
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            setDateTimeUpDown.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month,DateTime.Now.Day,DateTime.Now.Hour,0,0);            
+        }
+
+        private void BTNSetFolder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                settedPath = folderBrowserDialog.SelectedPath;
+        }
+
+        private void BTNStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (setDateTimeUpDown.Value != null)
+            {
+                settedDateTime = (DateTime)setDateTimeUpDown.Value;
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show($"Please, set the DateTime!", "DateTime error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DialogResult dialogResult = System.Windows.Forms.MessageBox.Show($"Program will delete all non-actual files at \'{settedPath}\' at date-time \'{settedDateTime.ToString()}\'",
+                "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == System.Windows.Forms.DialogResult.No)
+                return;
+
+            if (Directory.Exists(settedPath))
+            {
+                try
+                {
+                    string format = "yyMMdd-HHmm"; //don't mix up: MM - month; mm - minutes; hh - a.m./p.m. time; HH - military time
+                    CultureInfo provider = CultureInfo.InvariantCulture;     
+
+                    //Create list that contains filtered(deleted) XML files.
+                    List<String> xmlFileNameListFiltered = Directory.GetFiles(settedPath, "*.xml").ToList()
+                        .Where((s, res) => 
+                        (((DateTime.ParseExact(System.IO.Path.GetFileName(s).Substring(0, 11), format, provider).CompareTo(settedDateTime) <= 0)
+                        &&(DateTime.ParseExact(System.IO.Path.GetFileName(s).Substring(12, 11), format, provider).CompareTo(settedDateTime) >= 0)
+                        ||((DateTime.ParseExact(System.IO.Path.GetFileName(s).Substring(0, 11), format, provider).CompareTo(settedDateTime) >= 0)))) ? false : true)
+                         .ToList<string>();
+
+                    //Remove files that are in xmlFileNameListFiltered
+                    foreach (string s in xmlFileNameListFiltered)
+                        File.Delete(s);
+
+                    System.Windows.Forms.MessageBox.Show($"Removed files:{xmlFileNameListFiltered.Count}", "Non-actual files removed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex) // lazy exception
+                {
+                    System.Windows.Forms.MessageBox.Show($"The process failed: {ex.ToString()}", "Actualisation process failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else System.Windows.Forms.MessageBox.Show($"Folder \'{settedPath}\' not found!", "Folder not found",MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        
     }
 }
---
+++ TimeActualisationXMLTool/Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace TimeActualisationXMLTool
+{
+    class Model : INotifyPropertyChanged
+    {
+        private string _path; //a folder that must must be processed
+        private DateTime _datetime; //datetime to filter files
+
+        public string Path
+        {
+            get { return _path; }
+            set
+            {
+                _path = value;
+                OnPropertyChanged("Path");
+            }
+        }
+
+        public DateTime DateTime
+        {
+            get { return _datetime; }
+            set
+            {
+                _datetime = value;
+                OnPropertyChanged("DateTime");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
